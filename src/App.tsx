6ae229b7cab8846e013bef14089f27dae3fa9e94
danielgiftaided <daniel@giftaided.com>
2026-01-16 13:37:30 +0000

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/login'
+import Dashboard from './pages/dashboard'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
