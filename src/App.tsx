4b30d301cdd068cd74a98f243b9daf6051871c6a
danielgiftaided <daniel@giftaided.com>
2026-06-11 17:47:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/login'
 import Signup from './pages/signup'
+import ForgotPassword from './pages/forgotPassword'
+import ResetPassword from './pages/resetPassword'
 import Dashboard from './pages/dashboard'
 import Submissions from './pages/submissions'
 import SubmissionDetail from './pages/submissionDetail'
@@ -13,6 +15,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/submissions" element={<Submissions />} />
       <Route path="/submissions/:id" element={<SubmissionDetail />} />
